7f790388da3a3d535a95b4713c053205683bf376
Henrique Chaves <henriquechavesmm@gmail.com>
2021-09-14 20:33:48 -0300

    Create AddUser form and tests

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -4,14 +4,20 @@ import ReactDOM from 'react-dom';
 import axios from "axios";
 
 import UsersList from './components/UsersList';
+import AddUser from "./components/AddUser";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      users: []
+      users: [],
+      username: "",
+      email: ""
     }
+
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -24,15 +30,43 @@ class App extends Component {
       .catch(err => { console.log(err); })
   }
 
+  addUser(event) {
+    event.preventDefault();
+
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+
+    axios.post(`${process.env.REACT_APP_API_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({ username: "", email: "" });
+      })
+      .catch((err) => { console.log(err); });
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br />
-              <h1 className="title is-1 is-1">Users</h1>
+              <h1 className="title is-1">Users</h1>
               <hr /><br />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange} />
+              <br /><br />
               <UsersList users={this.state.users} />
             </div>
           </div>
