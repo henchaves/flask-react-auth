6655b97728005d666f65eb7385ca908836fd6920
Henrique Chaves M. de Menezes <henriquechavesmm@gmail.com>
2021-11-06 22:32:20 -0300

    Hash a password after changing it on Flask Admin

--- services/users/manage.py
+++ services/users/manage.py
@@ -8,17 +8,20 @@ from src.api.users.models import User
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+
 @cli.command("recreate_db")
 def recreate_db():
-  db.drop_all()
-  db.create_all()
-  db.session.commit()
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 @cli.command("seed_db")
 def seed_db():
-  db.session.add(User(username="michael", email="hermanmu@gmail.com"))
-  db.session.add(User(username="michaelherman", email="michael@mherman.org"))
-  db.session.commit()
+    db.session.add(User(username="michael", email="hermanmu@gmail.com", password="myfirstpassword"))
+    db.session.add(User(username="michaelherman", email="michael@mherman.org", password="mysecondpassword"))
+    db.session.commit()
+
 
 if __name__ == "__main__":
-  cli()
+    cli()
