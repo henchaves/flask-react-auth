2b44ef17d76e113af40884b11bdbdd485652ed69
Henrique Chaves <henriquechavesmm@gmail.com>
2022-01-13 23:19:10 -0300

    Update snapshots of LoginForm and RegisterForm

--- services/client/src/components/__tests__/LoginForm.test.js
+++ services/client/src/components/__tests__/LoginForm.test.js
@@ -3,11 +3,32 @@ import { render, cleanup } from "@testing-library/react";
 
 import LoginForm from "../LoginForm";
 
+const props = {
+  handleLoginFormSubmit: () => {
+    return true
+  }
+}
+
 afterEach(cleanup);
 
 it("renders properly", () => {
   const { getByText } = render(<LoginForm />);
   expect(getByText("Log In")).toHaveClass("title");
+});
+
+it("renders with default props", () => {
+  const { getByText, getByLabelText } = render(<LoginForm {...props} />);
+
+  const emailInput = getByLabelText("Email");
+  expect(emailInput).toHaveAttribute("type", "email");
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "password");
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText("Submit");
+  expect(buttonInput).toHaveValue("Submit");
 });
 
 it("renders", () => {
